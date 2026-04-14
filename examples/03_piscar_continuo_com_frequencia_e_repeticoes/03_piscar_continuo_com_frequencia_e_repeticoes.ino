87e1474303fd43e66d335ffb85673588913cf370
kauacordeirodev <kauacordeirodev@gmail.com>
2026-04-14 18:55:13 -0300

    :sparkles: feat: Adiciona terceiro exemplo de utilização biblioteca

---
+++ examples/03_piscar_continuo_com_frequencia_e_repeticoes/03_piscar_continuo_com_frequencia_e_repeticoes.ino
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "LED.h"
+
+Led ledVermelho(2);
+
+void setup() {
+  // Inicia o modo de piscar (frequência: 2Hz)
+  // Pisca 10 vezes completas, o LED desliga após a finalização das repetições.
+  ledVermelho.piscar(2, 10);
+}
+
+void loop() {
+  // Atualiza o estado do LED
+  ledVermelho.update();
+}
